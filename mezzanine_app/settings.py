fe2c8d8e9adc9ea4de68208d92a886965d7b8262
gaurav <grvtyagi22@gmail.com>
2016-11-18 15:28:57 +0530

    set some settings to remove warn run time

--- mezzanine_app/settings.py
+++ mezzanine_app/settings.py
@@ -198,6 +198,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
+            os.path.join(PROJECT_ROOT, "nova/templates"),
             os.path.join(PROJECT_ROOT, "templates")
         ],
         "APP_DIRS": True,
