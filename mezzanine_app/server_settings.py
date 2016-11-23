e7110955460b1ca987c4e36209ab3658e787ed78
Ubuntu <ubuntu@ip-172-31-24-40.ap-southeast-1.compute.internal>
2016-11-23 06:32:17 +0000

    integrate mezzanine with django equest.co

--- mezzanine_app/server_settings.py
+++ mezzanine_app/server_settings.py
@@ -18,8 +18,8 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'blog',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'root',
-            'PASSWORD': 'root',
+            'USER': 'eq-blog',
+            'PASSWORD': '@eq-blog@92',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
         }
--- mezzanine_app/wsgi.py
+++ mezzanine_app/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/home/ubuntu/blog-env/equest-blog/')
+sys.path.append('/home/ubuntu/blog-env/lib/python2.7/site-packages/')
 
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
